68016444019d96c21acc52137490617e117d343d
Timur <you@example.com>
2025-06-22 22:21:14 +0300

    main ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000', // или нужный фронтенд-адрес
+    origin: true, // или нужный фронтенд-адрес
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001);
