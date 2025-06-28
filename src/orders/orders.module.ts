ef95262f9f108993599684ff72ef3f9b59497411
Timur <you@example.com>
2025-06-28 14:02:17 +0300

    fix: add PrismaService to OrdersModule providers

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { PaymentModule } from '../payment/payment.module';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   imports: [PaymentModule],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, PrismaService],
   exports: [OrdersService],
 })
 export class OrdersModule {}
