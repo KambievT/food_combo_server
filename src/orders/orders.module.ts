e8fae87018915f7f97dc6d56929a5d23fb0ba875
Timur <you@example.com>
2025-06-29 20:39:48 +0300

    fix: import AuthModule in OrdersModule for guard dependencies

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,9 +3,10 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { PaymentModule } from '../payment/payment.module';
 import { PrismaService } from '../prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PaymentModule],
+  imports: [PaymentModule, AuthModule],
   controllers: [OrdersController],
   providers: [OrdersService, PrismaService],
   exports: [OrdersService],
